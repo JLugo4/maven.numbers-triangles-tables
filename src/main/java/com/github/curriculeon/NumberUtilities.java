23013669f42b7bf0b22f834d765181cbbf9fdbc0
JLugo4 <jarek.lugo96@gmail.com>
2023-08-28 17:13:39 -0400

    Started Num util

--- src/main/java/com/github/curriculeon/NumberUtilities.java
+++ src/main/java/com/github/curriculeon/NumberUtilities.java
@@ -3,57 +3,106 @@ package com.github.curriculeon;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
+
         String result = "";
+
         for (int i = start; i < stop; i++) {
+
             if(i % 2 == 0)
+
                 result += i;
+
         }
+
         return result;
+
     }
 
 
     public static String getOddNumbers(int start, int stop) {
+
         String result = "";
+
         for (int i = start; i < stop; i++) {
+
             if(i % 2 == 1)
+
                 result += i;
+
         }
+
         return result;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
-    }
 
-    public static String getRange(int start) {
         String result = "";
-        int currentNumber = 0;
-        while (currentNumber < start){
-            result += currentNumber;
-            currentNumber += 1;
+
+        int currentNumber = start;
+
+        while (currentNumber < stop){
+
+            int square = currentNumber * currentNumber;
+
+            result += square;
+
+            currentNumber += step;
         }
         return result;
     }
 
-    public static String getRange(int start, int stop) {
+    public static String getRange(int start) {
+
         String result = "";
-        int currentNumber = start;
-        while(currentNumber < stop) {
+
+        int currentNumber = 0;
+
+        while (currentNumber < start){
+
             result += currentNumber;
+
             currentNumber += 1;
+
+        }
+
+        return result;
+
+    }
+
+    public static String getRange(int start, int stop) {
+
+        String result = "";
+
+        int currentNumber = start;
+
+        while(currentNumber < stop) {
+
+            result += currentNumber;
+
+            currentNumber += 1;
+
             }
+
             return result;
+
     }
 
 
     public static String getRange(int start, int stop, int step) {
+
         String result = "";
+
         int currentNumber = start;
+
         while(currentNumber < stop) {
+
             result += currentNumber;
+
             currentNumber += step;
+
         }
+
         return result;
     }
 
