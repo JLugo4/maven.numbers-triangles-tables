7226f488b591340bd16c8c887c23e1ddfa4ce09c
JLugo4 <jarek.lugo96@gmail.com>
2023-08-29 18:14:38 -0400

    Triangle utils

--- src/main/java/com/github/curriculeon/TriangleUtilities.java
+++ src/main/java/com/github/curriculeon/TriangleUtilities.java
@@ -3,18 +3,39 @@ package com.github.curriculeon;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        //Create a variable to store the 'result' to return later
+        String result = "";
+            // Start looping 'numberOfRow' times
+        for (int i = 0; i < numberOfRows; i++) {
+        
+            // within each iteration create a row with 'iteration number' of star
+            String row = getRow(numberOfRows);
+            // add the newly created row to the 'result'
+            result += row + "\n";
+            //finish loop
+        }
+                // return result
+        return result;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        // create a result(Variable) to add stars too
+        String result = "";
+        String star = "*";
+            // Have a loop to add to the result a string '*' to then store in result
+        for (int i = 0; i < numberOfStars; i++) {
+            result += star;
+        }
+
+        return result;
     }
 
     public static String getSmallTriangle() {
-        return null;
+
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
